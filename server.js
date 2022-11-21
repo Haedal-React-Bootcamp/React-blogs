f6e7fd6637469e03f5a72b85353023a78518b678
WhalesBob <ndw8200@naver.com>
2022-11-21 21:34:28 +0900

    init : start to work with mysql

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mysql = require('mysql')
+const conn = {
+    host : '127.0.0.1',
+    port : '3306', 
+    user : 'root', // 임시
+    password : '1234', // 임시
+    database : 'test' // 임시
+}
+/*
+let connection = mysql.createConnection(conn);
+connection.connect();
+
+let sql = "INSERT INTO 'members' ('username','password') VALUES('test','test');";
+
+connection.query(sql, function(err,results, fields){
+    if(err){
+        console.log(err);
+    }
+    console.log(results);
+})
 
 app.listen(8080, function(){
     console.log('listening on 8080')
 })
+*/
 
 app.use(express.static(path.join(__dirname, 'react-project/build')));
 
