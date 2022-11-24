d11eeb4a3a85a210415c0cfec6b11e012423767c
남동우 <ndw8200@naver.com>
2022-11-24 00:03:34 +0000

    feat : update initial setting query of MySQL

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+
 /*
 const mysql = require('mysql')
 const conn = {
     host : '127.0.0.1',
     port : '3306', 
-    user : 'root', // 임시
-    password : '1234', // 임시
-    database : 'test' // 임시
+    user :  'forCommentDB',
+    password : '1234', 
+    database : 'comments_db'
 }
 
 let connection = mysql.createConnection(conn);
 connection.connect();
+
 
 let sql = "INSERT INTO 'members' ('username','password') VALUES('test','test');";
 
@@ -21,10 +23,6 @@ connection.query(sql, function(err,results, fields){
         console.log(err);
     }
     console.log(results);
-})
-
-app.listen(8080, function(){
-    console.log('listening on 8080')
 })
 */
 
